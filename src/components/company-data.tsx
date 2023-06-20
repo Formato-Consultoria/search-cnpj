3ffbf42e8e5b35ce105e33f62afd359f881b4384
Diego C. Silva <diegocaetano444@outlook.com>
2023-06-20 17:14:39 -0300

    update

--- src/components/company-data.tsx
+++ src/components/company-data.tsx
@@ -3,7 +3,7 @@ import { PropsDataCompany } from "@/@types/data-company";
 export default function BoxCompanyData(props: PropsDataCompany | any, dontChecked: boolean) {
     return (
         <div className={`${dontChecked ? "bg-green-500"  : "bg-zinc-200"}`}>
-            {props}
+            <pre>{JSON.stringify(props, null, 2)}</pre>
         </div>
     )
 }
