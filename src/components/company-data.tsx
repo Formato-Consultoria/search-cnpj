d26a714176dce7e3cb1c9c37efc3ec79ec0213c4
Diego C. Silva <diegocaetano444@outlook.com>
2023-06-20 17:58:25 -0300

    update

--- src/components/company-data.tsx
+++ src/components/company-data.tsx
@@ -1,14 +1,13 @@
 import { PropsDataCompany } from "@/@types/data-company";
 
-export default function BoxCompanyData(props: any, checked: boolean) {
+export default function BoxCompanyData(props: any) {
     return (
         <div
             className={`
-                w-full md:w-[768px] mx-5 ring-1 rounded-md px-5 py-10 overflow-auto
-                ${checked ?
+                w-full md:w-[768px] mx-5 ring-1 rounded-md px-5 py-10 overflow-auto bg-white/50
+                ${props.checked ?
                     "text-white bg-green-600 ring-zinc-200"
-                :
-                    "text-zinc-900 bg-zinc-200 ring-zinc-300"}
+                : "text-zinc-900 bg-zinc-200 ring-zinc-300"}
             `}
         >
             <pre>{JSON.stringify(props, null, 2)}</pre>
